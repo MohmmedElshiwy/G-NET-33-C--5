d7565f2f451422918308b2ac4522fcd42545fd99
MohmmedElshiwy <mohammed6elshiwy@gmail.com>
2026-02-21 00:22:03 +0300

    Problem 3 solutions

--- Program.cs
+++ Program.cs
@@ -112,3 +112,100 @@
 // #endregion
 
 // #endregion
+
+
+// #region Fnction
+
+//     #region Basic Calculator Functions
+//     static double Add(double a, double b)
+//     {
+//         return a + b;
+//     }
+
+//     static double Subtract(double a, double b)
+//     {
+//         return a - b;
+//     }
+
+//     static double Multiply(double a, double b)
+//     {
+//         return a * b;
+//     }
+
+//     static double Divide(double a, double b)
+//     {
+//         if (b == 0)
+//         {
+//             Console.WriteLine("Error: Cannot divide by zero!");
+//             return double.NaN; // Not a Number
+//         }
+
+//         return a / b;
+//     }
+
+
+
+//       Console.Write("Enter first number: ");
+//         double num1 = double.Parse(Console.ReadLine());
+
+//         Console.Write("Enter second number: ");
+//         double num2 = double.Parse(Console.ReadLine());
+
+//         Console.Write("Enter operation (+, -, *, /): ");
+//         char op = Console.ReadLine()[0];
+
+//         double result = 0;
+
+//         switch (op)
+//         {
+//             case '+':
+//                 result = Add(num1, num2);
+//                 break;
+
+//             case '-':
+//                 result = Subtract(num1, num2);
+//                 break;
+
+//             case '*':
+//                 result = Multiply(num1, num2);
+//                 break;
+
+//             case '/':
+//                 result = Divide(num1, num2);
+//                 break;
+
+//             default:
+//                 Console.WriteLine("Invalid operation!");
+//                 return;
+//         }
+
+//         Console.WriteLine($"Result = {result}");
+    
+        
+//     #endregion
+ 
+
+
+
+
+//     #region Circle Calculator
+//     static void CalculateCircle(double radius, out double area, out double circumference)
+//     {
+//         area = Math.PI * radius * radius;
+//         circumference = 2 * Math.PI * radius;
+//     }
+//     Console.Write("Enter circle radius: ");
+//         double radius = double.Parse(Console.ReadLine());
+
+//         double area;
+//         double circumference;
+
+//         CalculateCircle(radius, out area, out circumference);
+
+//         Console.WriteLine($"Area = {area}");
+//         Console.WriteLine($"Circumference = {circumference}");
+    
+        
+//     #endregion
+// #endregion
+
